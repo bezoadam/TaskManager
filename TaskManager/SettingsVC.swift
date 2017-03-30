c26814675f6bb2099b158485310b4c36d662a50f
Adam Bezak <bezoadam95@gmail.com>
2017-03-30 11:31:08 +0200

    settings

--- TaskManager/SettingsVC.swift
+++ TaskManager/SettingsVC.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class SettingsVC: UIViewController {
-
+class SettingsVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +21,34 @@ class SettingsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var colors = [UIColor.red, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.cyan]
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.colors.count
     }
-    */
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath)
+        
+        cell.backgroundColor = self.colors[indexPath.item]// make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
 
 }
