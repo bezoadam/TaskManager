3cf5f7f5d737391feeca80ebdbbfa4834b260e86
Adam Bezak <bezoadam95@gmail.com>
2017-03-30 14:17:52 +0200

    pridanvanie kategorii

--- TaskManager/SettingsVC.swift
+++ TaskManager/SettingsVC.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class SettingsVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var categoryName: UITextField!
     
     @IBOutlet weak var TableView: UITableView!
+    
+    var categories: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +25,113 @@ class SettingsVC: UIViewController, UICollectionViewDataSource, UICollectionView
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Categories")
+        
+        //3
+        do {
+            categories = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    var categories = CategoryObject()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.category_names.count
+        return self.categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
-        let row = indexPath.row
-        cell.textLabel?.text = categories.category_names[row]
-        cell.backgroundColor = categories.category_color[row].withAlphaComponent(0.2)
+        let name = self.categories[indexPath.row]
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "TableCell",
+                                          for: indexPath)
+        cell.textLabel?.text = name.value(forKeyPath: "name") as? String
+        cell.backgroundColor = name.value(forKey: "color") as? UIColor
         cell.isUserInteractionEnabled = false
 
         return cell
+    }
+    
+    @IBAction func addCategory(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "New catogory",
+                                      message: "Add a new category name",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            
+            guard let textField = alert.textFields?.first,
+                let categoryToSave =  textField.text else {
+                    return
+            }
+            
+            self.save(name: categoryToSave)
+            self.TableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+        
+
+    }
+
+    func save(name: String) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Categories",
+                                       in: managedContext)!
+        
+        let category = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        category.setValue(name, forKeyPath: "name")
+        category.setValue(UIColor.red, forKey: "color")
+        
+        // 4
+        do {
+            try managedContext.save()
+            categories.append(category)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
     
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
