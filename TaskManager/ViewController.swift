1fc758136db0a1f5767b3832911ce025ac0ed092
Adam Bezak <bezoadam95@gmail.com>
2017-03-31 14:57:36 +0200

    table view metody

--- TaskManager/ViewController.swift
+++ TaskManager/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var newTaskToAdd: NewTask?
+    var tasks: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +24,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //nacitanie ulozenych kategorii
+        let managedContext = getContext()
+        
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Tasks")
+        
+        do {
+            self.tasks = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
     @IBAction func unwindToMainVC(segue: UIStoryboardSegue){
         if let _ = self.newTaskToAdd {
             
         }
     }
 
+    //MARK TableView methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let name = self.tasks[indexPath.row]
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "TableCell",
+                                          for: indexPath)
+        cell.textLabel?.text = name.value(forKeyPath: "name") as? String
+        cell.selectionStyle = .none
+        cell.isUserInteractionEnabled = true
+        
+        return cell
+    }
 }
 
